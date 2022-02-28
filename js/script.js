5a8a313840e0abeb878ccaede9be8e6c0901dff6
Piotr Kozak <piotrekkozak9@gmail.com>
2022-02-28 20:03:04 +0100

    End handler event

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@
   });*/
 
 const titleClickHandler = function (event) {
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
 
@@ -16,7 +17,7 @@ const titleClickHandler = function (event) {
         activeLink.classList.remove('active');
     }
 
-    /* [IN PROGRESS] add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
 
     clickedElement.classList.add('active');
     console.log('clickedElement:', clickedElement);
@@ -28,11 +29,19 @@ const titleClickHandler = function (event) {
         activeArticle.classList.remove('active');
     }
 
-    /* get 'href' attribute from the clicked link */
+    /* [DONE] get 'href' attribute from the clicked link */
 
-    /* find the correct article using the selector (value of 'href' attribute) */
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
 
-    /* add class 'active' to the correct article */
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
+
+    /* [DONE] add class 'active' to the correct article */
+
+    targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
